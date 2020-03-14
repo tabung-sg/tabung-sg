bc36663adfe1751422a6d152602ab406cfd3f20d
Nur Muhammad Bin Sirat <mat.ace@gmail.com>
2020-03-14 23:03:34 +0800

    Generated devise: user.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admin_users, path: 'admin_users'
+  devise_for :users, path: 'users'
 
   root 'landing_page#index'
   resource 'mosques', only: [:index, :show]
