0e1467da815c78575794fda088c696d042a61759
Nur Muhammad Bin Sirat <mat.ace@gmail.com>
2020-03-14 23:20:35 +0800

    Set up devise with 2 models: User & AdminUser.
    All steps were performed following this wiki:
    
    https://github.com/heartcombo/devise/wiki/How-to-Setup-Multiple-Devise-User-Models

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, path: 'admin_users'
-  devise_for :users, path: 'users'
+  devise_for :admin_users, path: 'admin_users', controllers: {
+    sessions: 'admin_users/sessions',
+    registrations: 'admin_users/registrations',
+    passwords: 'admin_users/passwords',
+    confirmations: 'admin_users/confirmations',
+    unlocks: 'admin_users/unlocks'
+  }
+  devise_for :users, path: 'users', controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords',
+    confirmations: 'users/confirmations',
+    unlocks: 'users/unlocks'
+  }
 
   root 'landing_page#index'
   resource 'mosques', only: [:index, :show]
