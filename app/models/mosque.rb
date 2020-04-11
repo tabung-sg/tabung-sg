3210abce1759774e519e0e909cc81b3917b41daf
Muhammad Mohsin <muhammadmohsin@Muhammads-MacBook-Pro.local>
2020-04-11 21:19:20 +0800

    Updated the ActiveAdmin mosque new and show page to allow the uploading of QR codes.

--- app/models/mosque.rb
+++ app/models/mosque.rb
@@ -1,5 +1,5 @@
 class Mosque < ApplicationRecord
-    validates :name, :full_address, :phone, presence:true
+    validates :name, :full_address, :phone, presence: true
 
     has_one_attached :qr_code_image
 end
