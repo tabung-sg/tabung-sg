b0b42a2dea2c1d772b86ab744aed23ae65ea850c
Nur Muhammad Bin Sirat <mat.ace@gmail.com>
2020-04-15 14:28:50 +0800

    Calling /mosques.json returns image and qr_code as well.

--- app/controllers/mosques_controller.rb
+++ app/controllers/mosques_controller.rb
@@ -1,9 +1,22 @@
 class MosquesController < ApplicationController
+  before_action :set_host_for_local_storage
+
   def index
-    mosques = Mosque.all
+    mosques = Mosque.all.map do |m|
+      m.attributes.merge({
+                             image: url_for(m.image),
+                             qr_code: url_for(m.qr_code_image)
+                         })
+    end
 
     respond_to do |format|
       format.json { render json: {mosques: mosques}.to_json }
     end
+  end
+
+  private
+
+  def set_host_for_local_storage
+    ActiveStorage::Current.host = request.base_url if Rails.application.config.active_storage.service == :local
   end
 end
